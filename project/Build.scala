a1ca89526a7d729cca58da9bc0b57eb1fbbe60d2
Tsuyoshi Yoshizawa <tsuyoshi.yoshizawa@gmail.com>
2014-02-20 21:28:51 +0900

    version 0.4.0

--- project/Build.scala
+++ project/Build.scala
@@ -4,7 +4,7 @@ import Keys._
 object ScalaOAuth2Build extends Build {
 
   lazy val _organization = "com.nulab-inc"
-  lazy val _version =  "0.3.0"
+  lazy val _version =  "0.4.0"
   lazy val _playVersion =  "2.1.0"
 
   val _scalaVersion = "2.10.2"
