b39b83fedc6313aede41f27fa97a38b18e9153f2
Tsuyoshi Yoshizawa <tsuyoshi.yoshizawa@gmail.com>
2014-05-21 13:36:40 +0900

    version 0.7.0

--- project/Build.scala
+++ project/Build.scala
@@ -4,7 +4,7 @@ import Keys._
 object ScalaOAuth2Build extends Build {
 
   lazy val _organization = "com.nulab-inc"
-  lazy val _version =  "0.6.1"
+  lazy val _version =  "0.7.0"
   def _playVersion(version: String) = version match {
     case "2.11.0" => "2.3.0-RC1"
     case _ => "2.2.3"
