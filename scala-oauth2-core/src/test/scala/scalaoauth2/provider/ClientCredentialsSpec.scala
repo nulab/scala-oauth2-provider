2572f92273e2fe4673089bd8cfe8e9ee2298b687
Ryan Means <ryan@rmeans.com>
2014-08-05 23:41:00 -0500

    First pass on moving the library to support a non-blocking data-handler trait.
    Additional Comments:
    All tests passing, but probably needs a closer inspection particularly on some of the grants that throw exceptions as I don't believe those are currently being tested for each exception type.

--- scala-oauth2-core/src/test/scala/scalaoauth2/provider/ClientCredentialsSpec.scala
+++ scala-oauth2-core/src/test/scala/scalaoauth2/provider/ClientCredentialsSpec.scala
@@ -3,17 +3,24 @@ package scalaoauth2.provider
 import org.scalatest._
 import org.scalatest.Matchers._
 
+import scala.concurrent.Await
+import scala.concurrent.duration._
+import scala.concurrent.Future
+
 class ClientCredentialsSpec extends FlatSpec {
 
   it should "handle request" in {
     val clientCredentials = new ClientCredentials(new MockClientCredentialFetcher())
     val request = AuthorizationRequest(Map(), Map("scope" -> Seq("all")))
-    val grantHandlerResult = clientCredentials.handleRequest(request, new MockDataHandler() {
+    val grantHandlerResultFuture = clientCredentials.handleRequest(request, new MockDataHandler() {
 
-      override def findClientUser(clientId: String, clientSecret: String, scope: Option[String]): Option[MockUser] = Some(MockUser(10000, "username"))
+      override def findClientUser(clientId: String, clientSecret: String, scope: Option[String]): Future[Option[MockUser]] = Future.successful(Some(MockUser(10000, "username")))
 
-      override def createAccessToken(authInfo: AuthInfo[MockUser]): AccessToken = AccessToken("token1", None, Some("all"), Some(3600), new java.util.Date())
+      override def createAccessToken(authInfo: AuthInfo[MockUser]): Future[AccessToken] = Future.successful(AccessToken("token1", None, Some("all"), Some(3600), new java.util.Date()))
     })
+
+    val grantHandlerResult = Await.result(grantHandlerResultFuture, 5.seconds)
+
     grantHandlerResult.tokenType should be ("Bearer")
     grantHandlerResult.accessToken should be ("token1")
     grantHandlerResult.expiresIn should be (Some(3600))
--- scala-oauth2-core/src/test/scala/scalaoauth2/provider/PasswordSpec.scala
+++ scala-oauth2-core/src/test/scala/scalaoauth2/provider/PasswordSpec.scala
@@ -3,18 +3,25 @@ package scalaoauth2.provider
 import org.scalatest._
 import org.scalatest.Matchers._
 
+import scala.concurrent.Await
+import scala.concurrent.duration._
+import scala.concurrent.Future
+
 class PasswordSpec extends FlatSpec {
 
   it should "handle request" in {
     val password = new Password(new MockClientCredentialFetcher())
     val request = AuthorizationRequest(Map(), Map("username" -> Seq("user"), "password" -> Seq("pass"), "scope" -> Seq("all")))
-    val grantHandlerResult = password.handleRequest(request, new MockDataHandler() {
+    val grantHandlerResultFuture = password.handleRequest(request, new MockDataHandler() {
 
-      override def findUser(username: String, password: String): Option[MockUser] = Some(MockUser(10000, "username"))
+      override def findUser(username: String, password: String): Future[Option[MockUser]] = Future.successful(Some(MockUser(10000, "username")))
 
-      override def createAccessToken(authInfo: AuthInfo[MockUser]): AccessToken = AccessToken("token1", Some("refreshToken1"), Some("all"), Some(3600), new java.util.Date())
+      override def createAccessToken(authInfo: AuthInfo[MockUser]): Future[AccessToken] = Future.successful(AccessToken("token1", Some("refreshToken1"), Some("all"), Some(3600), new java.util.Date()))
 
     })
+
+    val grantHandlerResult = Await.result(grantHandlerResultFuture, 5.seconds)
+
     grantHandlerResult.tokenType should be ("Bearer")
     grantHandlerResult.accessToken should be ("token1")
     grantHandlerResult.expiresIn should be (Some(3600))
