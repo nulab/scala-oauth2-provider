af82f5371c6d9e98eede1ae6ac68bfdf3816774c
Gertjan Assies <g.assies@thenewmotion.com>
2014-08-08 11:18:08 +0200

    Added InvalidUser Exception and have it returned when the Datahandler.findUser() returns None

--- scala-oauth2-core/src/main/scala/scalaoauth2/provider/OAuthException.scala
+++ scala-oauth2-core/src/main/scala/scalaoauth2/provider/OAuthException.scala
@@ -50,6 +50,12 @@ class InvalidGrant(description: String = "") extends OAuthError(401, description
 
 }
 
+class InvalidUser(description: String = "") extends OAuthError(401, description) {
+
+  override val errorType = "unknown_user_password_combination"
+
+}
+
 class UnsupportedGrantType(description: String = "") extends OAuthError(description) {
 
   override val errorType = "unsupported_grant_type"
