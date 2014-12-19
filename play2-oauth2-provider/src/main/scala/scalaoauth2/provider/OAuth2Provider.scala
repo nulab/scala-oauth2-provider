52df5db3525711a79d865fed21eb3b61d839b1c2
Benny Thompson <ben.d.thomps@gmail.com>
2014-12-19 12:00:38 -0600

    Replaced existing authorize method with action wrapper
    Since calling the method inside an action is done every time authorize is called, the build in ActionBuilder is used so syntax to authenticate users is simplified.

--- play2-oauth2-provider/src/main/scala/scalaoauth2/provider/OAuth2Provider.scala
+++ play2-oauth2-provider/src/main/scala/scalaoauth2/provider/OAuth2Provider.scala
@@ -78,6 +78,9 @@ trait OAuth2BaseProvider extends Results {
 /**
  * OAuth2Provider supports issue access token and authorize.
  *
+ * @tparam H play.api.mvc.Request has type.
+ * @tparam U set the type in AuthorizationHandler.
+ *
  * <h3>Create controller for issue access token</h3>
  * <code>
  * object OAuth2Controller extends Controller with OAuth2Provider {
@@ -105,12 +108,37 @@ trait OAuth2BaseProvider extends Results {
  * }
  * </code>
  */
-trait OAuth2Provider extends OAuth2BaseProvider {
+trait OAuth2Provider[U] extends OAuth2BaseProvider {
+  import scala.concurrent.ExecutionContext.Implicits.global
+
+  /**
+   * Define the data handler to process token interactions
+   * @return The data handler implementation to use for token and user interaction
+   */
+  def dataHandler: DataHandler[U]
+
+  case class SecureRequest[A](user: U, request: Request[A]) extends WrappedRequest[A](request)
+
+  object SecureRequest extends SecureRequestBuilder {
+    def apply[A]() = new SecureRequestBuilder()
+  }
+
+  /**
+   * Action to authorize an already created access token in ProtectedResourceHandler process and return the response to client.
+   */
+  class SecureRequestBuilder extends ActionBuilder[({type R[A] = SecureRequest[A]})#R] {
+    override def invokeBlock[A](request: Request[A], block: SecureRequest[A] => Future[Result]): Future[Result] = {
+      protectedResource.handleRequest(request, dataHandler).flatMap {
+        case Left(e) if e.statusCode == 400 => Future.successful(BadRequest.withHeaders(responseOAuthErrorHeader(e)))
+        case Left(e) if e.statusCode == 401 => Future.successful(Unauthorized.withHeaders(responseOAuthErrorHeader(e)))
+        case Right(authInfo) => block(SecureRequest(authInfo.user, request))
+      }
+    }
+  }
 
   /**
    * Issue access token in AuthorizationHandler process and return the response to client.
    *
-   * @param handler Implemented AuthorizationHandler for register access token to your system.
    * @param request Playframework is provided HTTP request interface.
    * @param ctx This context is used by TokenEndPoint.
    * @tparam A play.api.mvc.Request has type.
@@ -118,65 +146,12 @@ trait OAuth2Provider extends OAuth2BaseProvider {
    * @return Request is successful then return JSON to client in OAuth 2.0 format.
    *         Request is failed then return BadRequest or Unauthorized status to client with cause into the JSON.
    */
-  def issueAccessToken[A, U](handler: AuthorizationHandler[U])(implicit request: Request[A], ctx: ExecutionContext): Future[Result] = {
-    tokenEndpoint.handleRequest(request, handler).map {
+  def issueAccessToken[A, U](implicit request: Request[A], ctx: ExecutionContext): Future[Result] = {
+    tokenEndpoint.handleRequest(request, dataHandler).map {
       case Left(e) if e.statusCode == 400 => BadRequest(responseOAuthErrorJson(e)).withHeaders(responseOAuthErrorHeader(e))
       case Left(e) if e.statusCode == 401 => Unauthorized(responseOAuthErrorJson(e)).withHeaders(responseOAuthErrorHeader(e))
       case Right(r) => Ok(Json.toJson(responseAccessToken(r))).withHeaders("Cache-Control" -> "no-store", "Pragma" -> "no-cache")
     }
   }
 
-  /**
-   * Authorize to already created access token in ProtectedResourceHandler process and return the response to client.
-   *
-   * @param handler Implemented ProtectedResourceHandler for authenticate to your system.
-   * @param callback Callback is called when authentication is successful.
-   * @param request Playframework is provided HTTP request interface.
-   * @param ctx This contxt is used by ProtectedResource.
-   * @tparam A play.api.mvc.Request has type.
-   * @tparam U set the type in AuthorizationHandler.
-   * @return Authentication is successful then the response use your API result.
-   *         Authentication is failed then return BadRequest or Unauthorized status to client with cause into the JSON.
-   */
-  def authorize[A, U](handler: ProtectedResourceHandler[U])(callback: AuthInfo[U] => Future[Result])(implicit request: Request[A], ctx: ExecutionContext): Future[Result] = {
-    protectedResource.handleRequest(request, handler).flatMap {
-      case Left(e) if e.statusCode == 400 => Future.successful(BadRequest.withHeaders(responseOAuthErrorHeader(e)))
-      case Left(e) if e.statusCode == 401 => Future.successful(Unauthorized.withHeaders(responseOAuthErrorHeader(e)))
-      case Right(authInfo) => callback(authInfo)
-    }
-  }
-
 }
-
-/**
- * OAuth2AsyncProvider supports issue access token and authorize in asynchronous.
- *
- * <h3>Create controller for issue access token</h3>
- * <code>
- * object OAuth2Controller extends Controller with OAuth2AsyncProvider {
- *   def accessToken = Action.async { implicit request =>
- *     issueAccessToken(new MyDataHandler())
- *   }
- * }
- * </code>
- *
- * <h3>Register routes</h3>
- * <code>
- * POST /oauth2/access_token controllers.OAuth2Controller.accessToken
- * </code>
- *
- * <h3>Authorized</h3>
- * <code>
- * import scalaoauth2.provider._
- * object BookController extends Controller with OAuth2AsyncProvider {
- *   def list = Action.async { implicit request =>
- *     authorize(new MyDataHandler()) { authInfo =>
- *       val user = authInfo.user // User is defined on your system
- *       // access resource for the user
- *     }
- *   }
- * }
- * </code>
- */
-@deprecated("Use OAuth2Provider", "0.12.0")
-trait OAuth2AsyncProvider extends OAuth2Provider
