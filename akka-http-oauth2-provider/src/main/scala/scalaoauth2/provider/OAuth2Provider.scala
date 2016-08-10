6d7578adb5ca5c1741eaa5385c9eb56cf6af8449
Christophe Pache <chpache@gmail.com>
2016-08-10 21:24:15 +0200

    add more information to be stored within the AccessToken (#101)

--- akka-http-oauth2-provider/src/main/scala/scalaoauth2/provider/OAuth2Provider.scala
+++ akka-http-oauth2-provider/src/main/scala/scalaoauth2/provider/OAuth2Provider.scala
@@ -4,22 +4,21 @@ import akka.http.scaladsl.model.StatusCodes._
 import akka.http.scaladsl.server.Directives
 import akka.http.scaladsl.server.directives.Credentials
 import scalaoauth2.provider.OAuth2Provider.TokenResponse
-import spray.json.DefaultJsonProtocol
+import spray.json.{ JsValue, DefaultJsonProtocol }
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 import scala.util.{ Failure, Success }
 
 trait OAuth2Provider[U] extends Directives with DefaultJsonProtocol {
-  import OAuth2Provider.tokenResponseFormat
   import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
 
   val oauth2DataHandler: DataHandler[U]
 
   val tokenEndpoint: TokenEndpoint
 
-  def grantResultToTokenResponse(grantResult: GrantHandlerResult[U]): TokenResponse =
-    TokenResponse(grantResult.tokenType, grantResult.accessToken, grantResult.expiresIn.getOrElse(1L), grantResult.refreshToken.getOrElse(""))
+  def grantResultToTokenResponse(grantResult: GrantHandlerResult[U]): JsValue =
+    OAuth2Provider.tokenResponseFormat.write(TokenResponse(grantResult.tokenType, grantResult.accessToken, grantResult.expiresIn.getOrElse(1L), grantResult.refreshToken.getOrElse("")))
 
   def oauth2Authenticator(credentials: Credentials): Future[Option[AuthInfo[U]]] =
     credentials match {
@@ -39,7 +38,7 @@ trait OAuth2Provider[U] extends Directives with DefaultJsonProtocol {
             case Success(maybeGrantResponse) =>
               maybeGrantResponse.fold(
                 oauthError => complete(Unauthorized),
-                grantResult => complete(tokenResponseFormat.write(grantResultToTokenResponse(grantResult)))
+                grantResult => complete(grantResultToTokenResponse(grantResult))
               )
             case Failure(ex) => complete(InternalServerError, s"An error occurred: ${ex.getMessage}")
           }
