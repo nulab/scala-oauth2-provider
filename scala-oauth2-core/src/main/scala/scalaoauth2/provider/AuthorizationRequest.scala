ab4da9af914a5a400970e70e0c603e1f7f6c3d04
Tsuyoshi Yoshizawa <tsuyoshi.yoshizawa@gmail.com>
2016-09-05 00:34:03 +0900

    remove commons-codec from dependency (#104)

--- scala-oauth2-core/src/main/scala/scalaoauth2/provider/AuthorizationRequest.scala
+++ scala-oauth2-core/src/main/scala/scalaoauth2/provider/AuthorizationRequest.scala
@@ -1,50 +1,39 @@
 package scalaoauth2.provider
 
-import org.apache.commons.codec.binary.Base64
+import java.util.Base64
+
+import scala.util.Try
 
 case class ClientCredential(clientId: String, clientSecret: Option[String])
 
 class AuthorizationRequest(headers: Map[String, Seq[String]], params: Map[String, Seq[String]]) extends RequestBase(headers, params) {
 
-  /**
-   * Returns grant_type.
-   *
-   * OAuth defines four grant types:
-   * authorization code
-   * implicit
-   * resource owner password credentials, and client credentials.
-   *
-   * @return grant_type
-   */
-  def grantType: String = requireParam("grant_type")
-
-  /**
-   * Returns scope.
-   *
-   * @return scope
-   */
   def scope: Option[String] = param("scope")
 
-  lazy val clientCredential: Option[ClientCredential] = {
-    header("Authorization").flatMap {
-      """^\s*Basic\s+(.+?)\s*$""".r.findFirstMatchIn
-    } match {
-      case Some(matcher) =>
-        val authorization = matcher.group(1)
-        val decoded = new String(Base64.decodeBase64(authorization.getBytes), "UTF-8")
-        if (decoded.indexOf(':') > 0) {
-          decoded.split(":", 2) match {
-            case Array(clientId, clientSecret) => Some(ClientCredential(clientId, if (clientSecret == "") None else Some(clientSecret)))
-            case Array(clientId) => Some(ClientCredential(clientId, None))
-          }
-        } else {
+  def grantType: String = requireParam("grant_type")
+
+  lazy val clientCredential: Option[ClientCredential] =
+    findAuthorization
+      .flatMap(clientCredentialByAuthorization)
+      .orElse(clientCredentialByParam)
+
+  private def findAuthorization = for {
+    authorization <- header("Authorization")
+    matcher <- """^\s*Basic\s+(.+?)\s*$""".r.findFirstMatchIn(authorization)
+  } yield matcher.group(1)
+
+  private def clientCredentialByAuthorization(s: String) =
+    Try(new String(Base64.getDecoder.decode(s), "UTF-8"))
+      .map(_.split(":", 2))
+      .getOrElse(Array.empty) match {
+        case Array(clientId, clientSecret) =>
+          Some(ClientCredential(clientId, if (clientSecret.isEmpty) None else Some(clientSecret)))
+        case _ =>
           None
-        }
-      case _ => param("client_id").map { clientId =>
-        ClientCredential(clientId, param("client_secret"))
       }
-    }
-  }
+
+  private def clientCredentialByParam = param("client_id").map(ClientCredential(_, param("client_secret")))
+
 }
 
 case class RefreshTokenRequest(request: AuthorizationRequest) extends AuthorizationRequest(request.headers, request.params) {
