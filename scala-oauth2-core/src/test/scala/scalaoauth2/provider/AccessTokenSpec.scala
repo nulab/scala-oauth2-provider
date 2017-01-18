e46f4c4ab4b99a8afb54d356272a1c9fd375d7cc
Ryan Means <ryan.means@lifeway.com>
2017-01-18 11:38:37 -0600

    be more clear on which variables are being set.

--- scala-oauth2-core/src/test/scala/scalaoauth2/provider/AccessTokenSpec.scala
+++ scala-oauth2-core/src/test/scala/scalaoauth2/provider/AccessTokenSpec.scala
@@ -9,17 +9,17 @@ import org.scalatest._
 class AccessTokenSpec extends FlatSpec {
 
   it should "say a token is active that is not yet expired" in {
-    val token = AccessToken("token", None, None, Some(15), new Date())
+    val token = AccessToken("token", None, None, lifeSeconds = Some(15), createdAt = new Date())
     token.isExpired shouldBe false
   }
 
   it should "expire tokens that have a lifespan that has passed" in {
-    val token = AccessToken("token", None, None, Some(1798), Date.from(ZonedDateTime.now(ZoneOffset.UTC).minusMinutes(30).toInstant))
+    val token = AccessToken("token", None, None, lifeSeconds = Some(1798), createdAt = Date.from(ZonedDateTime.now(ZoneOffset.UTC).minusSeconds(1800).toInstant))
     token.isExpired shouldBe true
   }
 
   it should "not expire tokens that have no lifespan" in {
-    val token = AccessToken("token", None, None, None, Date.from(ZonedDateTime.now(ZoneOffset.UTC).minusMinutes(30).toInstant))
+    val token = AccessToken("token", None, None, lifeSeconds = None, createdAt = Date.from(ZonedDateTime.now(ZoneOffset.UTC).minusSeconds(1800).toInstant))
     token.isExpired shouldBe false
   }
 }
