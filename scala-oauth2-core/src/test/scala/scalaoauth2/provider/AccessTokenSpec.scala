dce99f426581bee1ffb7e47bfafb51ec89cbf07e
Ryan Means <ryan@rmeans.com>
2017-01-18 21:22:32 -0500

    Add expiration tests (#112)
    * Add tests for verifying logic on checking token expiration.
    
    * be more clear on which variables are being set.

---
+++ scala-oauth2-core/src/test/scala/scalaoauth2/provider/AccessTokenSpec.scala
@@ -0,0 +1,25 @@
+package scalaoauth2.provider
+
+import java.time.{ ZoneOffset, ZonedDateTime }
+import java.util.Date
+
+import org.scalatest.Matchers._
+import org.scalatest._
+
+class AccessTokenSpec extends FlatSpec {
+
+  it should "say a token is active that is not yet expired" in {
+    val token = AccessToken("token", None, None, lifeSeconds = Some(15), createdAt = new Date())
+    token.isExpired shouldBe false
+  }
+
+  it should "expire tokens that have a lifespan that has passed" in {
+    val token = AccessToken("token", None, None, lifeSeconds = Some(1798), createdAt = Date.from(ZonedDateTime.now(ZoneOffset.UTC).minusSeconds(1800).toInstant))
+    token.isExpired shouldBe true
+  }
+
+  it should "not expire tokens that have no lifespan" in {
+    val token = AccessToken("token", None, None, lifeSeconds = None, createdAt = Date.from(ZonedDateTime.now(ZoneOffset.UTC).minusSeconds(1800).toInstant))
+    token.isExpired shouldBe false
+  }
+}
